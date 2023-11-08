900f7e44c261d722808372ce77b91890fe93757e
sourav-maji <s722143@gmail.com>
2023-11-08 09:01:08 +0530

    part 2

--- 1stEp/index.js
+++ 1stEp/index.js
@@ -11,12 +11,26 @@
 // console.log("After",getName)
 
 
-var a =7
-console.log(a);
+// var a =7
+// console.log(a);
 
-var x;
+// var x;
 
-if (x === undefined) {
-    console.log(`Value of X is ${x} `);
+// if (x === undefined) {
+//     console.log(`Value of X is ${x} `);
+// }
+
+
+function a (){
+    function c (){
+        console.log(b);
+    }
+    c()
 }
+
+var b = 10;
+a();
+
+// Lexial env  means local memory of a variable + lexical memory of the parent of that variable
+// Scope Chain :- The whole chain of lexical env is called scope chain
 
